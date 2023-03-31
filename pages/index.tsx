12d7d07177862dec6c46abd8b574f1906fd1f8e0
Chan Lee <dlcks17@kakao.com>
2023-03-31 09:37:29 +0900

    download button aniamte modify

--- pages/index.tsx
+++ pages/index.tsx
@@ -317,8 +317,8 @@ export default function Home({data}:InferGetServerSidePropsType<typeof getServer
                                 setDownloading(true);
                                 downloadClick();
                               }}
-                              className="text-white cursor-pointer hover:animate-bounce">
-                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
+                              className="text-white cursor-pointer">
+                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10 hover:animate-bounce">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 13.5l3 3m0 0l3-3m-3 3v-6m1.06-4.19l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z" />
                               </svg>
                             </div>
